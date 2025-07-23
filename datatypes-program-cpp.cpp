d9bdc101462fbcb7e6bbc1e672e97f51545b2123
Vaishnavi Verma <vaiatsunrise@gmail.com>
2025-07-24 00:53:25 +0530

    Add files via upload

---
+++ datatypes-program-cpp.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    inta;
+    cout<<"Enter any integer: ";
+    cin>>a;
+    cout<<"Integer = "<<a<<" and size is "<<sizeof(a)<<" bytes"<<endl;
+
+    floatb;
+    cout<<"Enter any number with decimal: ";
+    cin>>b;
+    cout<<"Float = "<<b<<" and size is "<<sizeof(b)<<" bytes"<<endl;
+
+    stringc;
+    cout<<"Enter any word: ";
+    cin>>c;
+    cout<<"Srting = "<<c<<" and size is "<<sizeof(c)<<" bytes"<<endl;
+
+    doubled;
+    cout<<"Enter any number with bigger decimal: ";
+    cin>>b;
+    cout<<"Double = "<<d<<" and size is "<<sizeof(d)<<" bytes"<<endl;
+
+    chare;
+    cout<<"Enter any character: ";
+    cin>>e;
+    cout<<"Character = "<<e<<" and size is "<<sizeof(e)<<" bytes"<<endl;
+
+    boolf;
+    cout<<"Enter True/False: ";
+    cin>>boolalpha;
+    cin>>f;
+    cout<<"Boolean = "<<f<<" and size is "<<sizeof(f)<<" bytes"<<endl;
+
+    return 0
+}
